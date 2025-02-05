1325eed87821eaa19f814964d174cc1a6ce32220
Mohim-Singla <mohimsingla@gmail.com>
2025-02-05 17:53:00 +0530

    type(improvement): Strict schema validation

--- src/apiValidations/shortenUrl.js
+++ src/apiValidations/shortenUrl.js
@@ -1,11 +1,12 @@
 import Joi from 'joi';
+import { utility } from '../utils/index.js';
 
 const generateShortenUrl = {
   body: Joi.object({
     url: Joi.string().required(),
     expiry: Joi.object({
       value: Joi.number().required(),
-      unit: Joi.string().required(),
+      unit: Joi.string().valid(...utility.constant.EXPIRY_UNITS).required(),
     }).required(),
     prefer_existing_url: Joi.boolean().optional(),
   }),
--- src/utils/constant/index.js
+++ src/utils/constant/index.js
@@ -4,4 +4,5 @@ export const constant = {
     DEV: 'development',
     PROD: 'production',
   },
+  EXPIRY_UNITS: ['day', 'hour', 'minute', 'second'],
 };
